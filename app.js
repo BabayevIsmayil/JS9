c0461f55c0548a6b83e889f10f8afa2c58cb7506
BabayevIsmayil <bismayil2005@gmail.com>
2024-04-02 11:13:39 +0400

    JS9

---
+++ app.js
@@ -0,0 +1,9 @@
+var insanlar = [
+    { ad: "Ismayil", soyad: "Abbasov", yas: 23 },
+    { ad: "Ismayil", soyad: "Babayev", yas: 20 },
+    { ad: "Ismayil", soyad: "Cabbarov", yas: 17 }
+];
+
+for (var i = 0; i < insanlar.length; i++) {
+    console.log("Ad: " + insanlar[i].ad + ", Soyad: " + insanlar[i].soyad + ", Yaş: " + insanlar[i].yas);
+}
